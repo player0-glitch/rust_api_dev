45fba46a3a976cd052c8c6d47785d1c9003e3c1b
philippe <103938849+player0-glitch@users.noreply.github.com>
2024-11-10 10:13:59 +0200

    initial commit

--- src/schema.rs
+++ src/schema.rs
@@ -1,11 +1,2 @@
 // @generated automatically by Diesel CLI.
-diesel::table! {
-    albums (id){
-        id->Integer,
-        artist->Text,
-        year->Integer,
-        name ->Text,
-        is_released -> Bool
-    }
-    
-}
+
